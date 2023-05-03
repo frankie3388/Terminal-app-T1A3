1fc8d6f5ad8ab9d40823adb3d5e75ed667102574
frankie3388 <frankiejuen33@gmail.com>
2023-05-04 02:12:52 +1000

    added in second test

--- src/test_functions.py
+++ src/test_functions.py
@@ -1,6 +1,6 @@
 import pytest
 
-from functions import add_history, view_history, funds, play, display_result, betting, bet_selection
+from functions import add_history, view_history, funds, play, display_result, betting, bet_selection, win_lose
 
 # def test_basic():
 #     assert "hello" == "hello"
@@ -10,4 +10,7 @@ def test_funds():
     assert funds(user_input='100') == 100
 
 # def test_play():
-    
+
+def test_win_lose():
+    assert win_lose(["red"], 3, 50, 450, "red", "odd") == 550
+    assert win_lose(["red"], 3, 50, 550, "red", "odd") == 650
