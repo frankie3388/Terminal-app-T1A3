1015ddfe9dfda783ab8d3e97685ae4330e8d76a8
frankie3388 <frankiejuen33@gmail.com>
2023-05-03 15:54:47 +1000

    added conditions to bash script

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-python3 -m venv roulette-venv
-
-source roulette-venv/bin/activate
-pip3 install -r requirements.txt
-python3 main.py
+if [ -f "./roulette-venv/bin/activate" ]; then
+    source roulette-venv/bin/activate
+    pip3 install -r requirements.txt
+    python3 main.py
+else
+    python3 -m venv roulette-venv
+    source roulette-venv/bin/activate
+    pip3 install -r requirements.txt
+    python3 main.py
+fi
