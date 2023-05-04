9a18130211b7028ddae6f83cda540fd47e83019c
frankie3388 <frankiejuen33@gmail.com>
2023-05-04 17:59:46 +1000

    added in the 4th test

--- src/test_functions.py
+++ src/test_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import csv
 from functions import add_history, view_history, funds, play, display_result, betting, bet_selection, win_lose
 
 
@@ -14,4 +15,12 @@ def test_betting(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: 10)
     assert betting("red", 500) == 10
 
-   
+def test_view_history(capfd):
+    test_file = "mock_history.csv"
+    expected_output = "['3', 'red', 'odd']\n"
+    with open(test_file, "w") as history_file:
+        writer = csv.writer(history_file)
+        writer.writerow(['3', "red", "odd"])
+    view_history(test_file)
+    captured = capfd.readouterr()
+    assert captured.out == expected_output
